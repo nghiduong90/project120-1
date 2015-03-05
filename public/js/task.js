2c55fa10b80b159ec7e08f1e9812e1fe0bf6d60f
Nghi Duong <nghiduong90@gmail.com>
2015-03-05 01:45:06 +0000

    make it clear

--- public/js/task.js
+++ public/js/task.js
@@ -47,43 +47,74 @@ function initializePage() {
 			var display_date = date_obj.toLocaleDateString('en-US', options);
 
 			var details_div = $('#task' + idNumber + ' .details');
-			console.log ("img in task.js " + data_json['img']);
+			//console.log ("img in task.js " + data_json['img']);
 			var new_html = //'<p> print something in html  </p>'; 
 
 					'<div class="panel-footer">' +
-					'<span class="pull-left"><b>Date</b>:' + display_date + '</span>' +
-	                '<span class="pull-left"><b>Description</b>:' + data_json['description'] + '</span>' +
+					'<span><b>Date:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;</b>' + display_date + '</span><br>' +
+	                '<span><b>Description:&#160;&#160;</b>' + data_json['description'] + '</span><br>' +
 	                //'<span class="pull-left">' + data_json['img'] + '</span>' +
 	                //'<img class="img-thumbnail" src="' + data_json['img'] + '" alt="task">' +
-	                '<p class="tex-center">' + data_json['priority'] + '</p>' +
+	                //'<p class="tex-center">' + data_json['priority'] + '</p>' +
 	                //'<div class="clearfix"></div>' +
-	                '<div class="complete checkbox">' +
+	               	'<div class="row">' +
+	                	'<div class="col-lg-6 pull-left">' +
+	                		'<div class="complete checkbox">' +
+                    			'<label>' +
+                    				'<input type="checkbox">&#160;&#160;&#160;&#160;&#160;Completed This Task' +
+                    			'</label>' +
+                    		'</div>' +
+                    	'</div>' +
+                    	'<div class="col-lg-3 pull-right">' +
+	                		'<button class="project-delete btn btn-danger" '+
+								'type="button" style="float: right">delete</button> <br> <br>' +
+						'</div>' + 
+	                '</div>'; 
+
+
+	            /*    '<div class="complete checkbox">' +
                     '<label>' +
-                    '<input type="checkbox">Completed This Task' +
+                    '<input type="checkbox">&#160;&#160;&#160;&#160;&#160;Completed This Task' +
                     '</label>' +
                     '</div>' +
 	                '<button class="project-delete btn btn-danger" '+
 						'type="button" style="float: right">delete</button> <br> <br>' + 
 	                '</div>';   
-					//'<div class="project-summary">'+project_json['summary']+'</div>'+
+ */
 
-			//var details_div = $('#task' + idNumber + '.details');
-			//details_div.html('<p> print something in html  </p>');
-			if ((counter % 2) == 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+			if ((counter % 2) == 1) {
 				details_div.html(new_html);
+
+				// define action for delete
+				details_div.find('.project-delete').click(function(e){
+					$.post('/task/'+idNumber+'/delete', function() {
+					window.location.href = '/';
+					});
+				});
+
+				// define action for delete task
+				details_div.find('.complete').click(function(e){
+					$.get('/task/'+idNumber+'/complete', function() {
+						window.location.href = '/history';
+					});
+				});
+			}
 			else
 				details_div.html('');
-			details_div.find('.project-delete').click(function(e){
-				$.post('/task/'+idNumber+'/delete', function() {
-					window.location.href = '/';
-				});
-			} );
 
-		details_div.find('.complete').click(function(e){
-				$.get('/task/'+idNumber+'/complete', function() {
-					window.location.href = '/history';
-				});
-			} );
 		}
 
 		$.get(url_call, addTaskDetails);
