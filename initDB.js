46a10e878fe021f1282ffcac6c0df27e6dddb395
Nghi Duong <nghiduong90@gmail.com>
2015-10-06 20:32:46 +0000

    minor change

--- initDB.js
+++ initDB.js
@@ -18,7 +18,7 @@ var modelHistory = require('./history');
 // Connect to the Mongo database, whether locally or on Heroku
 // MAKE SURE TO CHANGE THE NAME FROM 'lab7' TO ... IN OTHER PROJECTS
 var local_database_name = 'project120-1';
-var local_database_uri  = 'mongodb://localhost/' + local_database_name
+var local_database_uri  = 'mongodb://localhost/' + local_database_name;
 var database_uri = process.env.MONGOLAB_URI || local_database_uri
 mongoose.connect(database_uri);
 
